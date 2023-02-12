cfcb105258e5cfedd6eb9357a860aac317ae161b
OUF5git config --global user.name OUF5 <219110250@psu.edu.sa>
2023-02-12 17:40:07 +0300

    Pushing jetskin.java

---
+++ jetskin.java
@@ -0,0 +1,17 @@
+public class JavaApplication105 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        int charPerLine=0; 
+        for (int i = 33; 1 < 256; i++, charPerLine++){
+            System.out.println((char)i+((charPerLine%10==0)?"\n":"\t"));
+            System.out.println();
+        
+    }
+
+        }
+
+}
